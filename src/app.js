f69105faabae0ccc03340fe38bc1692d39843551
zhangxiaolin <zhangxiaolin@sunniwell.net>
2020-07-30 23:44:15 +0800

    检测按钮组的button组件

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
+import GroupButton from './button-group.vue'
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group', GroupButton)
 
 new Vue({
     el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    for (const key of this.$el.children) {
+      if (key.nodeName.toLowerCase() !== "button") {
+        console.warn(`组件内只能包含button标签,不能包含${key.nodeName}`);
+        return false;
+      }
+    }
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.g-button-group {
+  display: flex;
+  > .g-button {
+    border-radius: 0;
+    margin-left: -1px;
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
