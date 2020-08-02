cfd517d5cf2859838d143806535975e61e2c1a16
943248247@qq.com <Zxlswns1998>
2020-08-02 13:52:54 +0800

    使用 持续集成 进行测试

--- src/app.js
+++ src/app.js
@@ -18,65 +18,74 @@ import chai from 'chai'
 import spies from 'chai-spies'
 chai.use(spies)
 const expect = chai.expect;
-{
-    const Constructor = Vue.extend(Button)
-    let button = new Constructor({
-        propsData: {
-            icon: 'setings'
-        }
+
+try {
+    {
+        const Constructor = Vue.extend(Button)
+        let button = new Constructor({
+            propsData: {
+                icon: 'setings'
+            }
+        })
+        button.$mount('#test')
+        let useElement = button.$el.querySelector('use')
+        // 期待 useElement的属性xlink:href的参数是#i-setings
+        expect(useElement.getAttribute('xlink:href')).to.eq('#i-setings')
+        button.$el.remove()
+        button.$destroy()
+    }
+    {
+        const Constructor = Vue.extend(Button)
+        let button = new Constructor({
+            propsData: {
+                icon: 'loading',
+                loading: true
+            }
+        })
+        button.$mount()
+        let useElement = button.$el.querySelector('use')
+        let href = useElement.getAttribute('xlink:href')
+        expect(href).to.eq('#i-loading')
+        button.$el.remove()
+        button.$destroy()
+    }
+    {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        let button = new Constructor({
+            propsData: {
+                icon: 'loading'
+            }
+        })
+        button.$mount(div)
+        let svg = button.$el.querySelector('svg')
+        let { order } = window.getComputedStyle(svg)
+        expect(order).to.eq('1')
+        button.$el.remove()
+        button.$destroy()
+    }
+    {
+        const Constructor = Vue.extend(Button)
+        let vm = new Constructor({
+            propsData: {
+                icon: 'setings'
+            }
+        })
+        vm.$mount()
+        // 使用函数监听
+        let spy = chai.spy(function () {
+        })
+        vm.$on('click', spy)
+        let button = vm.$el
+        button.click()
+        //期待触发click事件的时候 spy已经被调用了
+        expect(spy).to.have.been.called()
+    }
+} catch (error) {
+  window.errors = [error]
+} finally {
+   window.errors && window.errors.forEach((error)=>{
+        console.error(error.message)
     })
-    button.$mount('#test')
-    let useElement = button.$el.querySelector('use')
-    // 期待 useElement的属性xlink:href的参数是#i-setings
-    expect(useElement.getAttribute('xlink:href')).to.eq('#i-setings')
-    button.$el.remove()
-    button.$destroy()
-}
-{
-    const Constructor = Vue.extend(Button)
-    let button = new Constructor({
-        propsData: {
-            icon: 'loading',
-            loading: true
-        }
-    })
-    button.$mount()
-    let useElement = button.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-loading')
-    button.$el.remove()
-    button.$destroy()
-}
-{
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    let button = new Constructor({
-        propsData: {
-            icon: 'loading'
-        }
-    })
-    button.$mount(div)
-    let svg = button.$el.querySelector('svg')
-    let { order } = window.getComputedStyle(svg)
-    expect(order).to.eq('1')
-    button.$el.remove()
-    button.$destroy()
-}
-{
-    const Constructor = Vue.extend(Button)
-    let vm = new Constructor({
-        propsData: {
-            icon: 'setings'
-        }
-    })
-    vm.$mount()
-    // 使用函数监听
-    let spy = chai.spy(function () {
-    })
-    vm.$on('click', spy)
-    let button = vm.$el
-    button.click()
-    //期待触发click事件的时候 spy已经被调用了
-    expect(spy).to.have.been.called()
 }
--- src/button.vue
+++ src/button.vue
@@ -9,7 +9,12 @@
 </template>
 
 <script>
+  import Vue from 'vue'
+  import Icon from './icon'
 export default {
+  components:{
+    'g-icon': Icon
+  },
   props: {
     iconPosition: {
       type: String,
